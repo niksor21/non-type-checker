ee029043342b01394bc1850aa35f0622053a518b
Nick <niksor21@ya.ru>
2022-02-03 15:34:48 +0300

    non type checker

---
+++ main.py
@@ -0,0 +1,10 @@
+null_variable = None 
+not_null_variable = 'some_thing' 
+if null_variable == None: 
+    print('null_variable не определено') 
+else: 
+    print('null_variable определено') 
+if not_null_variable == None: 
+    print('not_null_variable не определено') 
+else: 
+    print('not_null_variable определено')
